dead9cc5072e48bf413aecbff4e286ff95936d5e
Chigozie Mbama Mykel <92220456+mbamamykel77@users.noreply.github.com>
2023-06-02 16:59:54 +0100

    added validator to edit user

--- src/controllers/updateUser.js
+++ src/controllers/updateUser.js
@@ -1,12 +1,17 @@
-// import { BadUserRequestError } from "../error/error.js";
+import { BadUserRequestError } from "../error/error.js";
 import User from "../model/user.model.js";
-// import { updateUserValidator } from "../validator/updateUser.validator.js";
+import { updateUserValidator } from "../validator/updateUser.validator.js";
 
 
 export default class UpdateController {
 static async updateUser (req, res) {
+    const { error } = updateUserValidator.validate(req.body)
+    if (error) {
+        return res.status(400).json({ error: error.message });
+    };
+
     const { id } = req.params;
-    const { fullName, email, phoneNumber, deliveryAddress } = req.body;
+    const { fullName, email, phoneNumber, deliveryAddress } = (req.body);
   
     try {
       const user = await User.findOneAndUpdate(
@@ -19,9 +24,16 @@ static async updateUser (req, res) {
         return res.status(404).json({ error: 'User not found' });
         // throw new BadUserRequestError ("user does not exist")
       }
+      res.status(200).json({
+        message: "User profile updated successfully",
+        status: "Success",
+        data: {
+          user,
+        },
+      });
 
 
-      return res.json({ message: 'User profile updated successfully', user });
+    //   return res.json({ message: 'User profile updated successfully', user });
     } catch (error) {
       console.error('Error updating user:', error);
       return res.status(500).json({ error: 'Internal server error' });
--- src/validator/updateUser.validator.js
+++ src/validator/updateUser.validator.js
@@ -1,10 +1,10 @@
 import Joi from "joi";
 
 export const updateUserValidator = Joi.object({
-    fullName: Joi.string().required(),
+    fullName: Joi.string().optional(),
     email: Joi.string()
       .regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
-      .required()
+      .optional()
       .messages({
         "string.pattern.base": "Email is not a valid email format/address",
       }),
@@ -12,13 +12,13 @@ export const updateUserValidator = Joi.object({
       .regex(
         /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{8,}$/
       )
-      .required()
+      .optional()
       .messages({
         "string.pattern.base":
           "You need one number, one alphanumeric character and one in caps, password be more than 8 characters long",
       }),
       phoneNumber: Joi.string()
-      .required()
+      .optional()
       .pattern(/^[0-9]{10}$/)
       .messages({
         "string.pattern.base": "Phone number must consist of 10 digit",
